56ce8c07308a34cf8010376daabf1953ab65340e
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2022-11-20 13:49:46 +0900

    Update install-dotnet6.sh

--- install-dotnet6.sh
+++ install-dotnet6.sh
@@ -1,18 +1,11 @@
 # Setup .NET 6 on WSL2
-# https://docs.microsoft.com/ja-jp/dotnet/core/install/linux-ubuntu#2004
+# https://learn.microsoft.com/ja-jp/dotnet/core/install/linux-ubuntu#2204
 
 #!/bin/bash
 set -xe
 
-sudo apt-get update -y
-
 # Install package
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-# Install SDK
 sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-6.0
+  sudo apt-get install -y dotnet6
 
 dotnet --list-sdks
