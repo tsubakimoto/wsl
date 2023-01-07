eddd99ff4a584470644b1d6dea4c399737b92bff
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2023-01-07 23:04:42 +0900

    update .net7 install

---
+++ install-dotnet7.sh
@@ -0,0 +1,12 @@
+# Setup .NET 7 on WSL2
+# https://github.com/dotnet/core/blob/main/release-notes/7.0/install-linux.md
+# https://github.com/dotnet/sdk/issues/28995#issuecomment-1310911847
+
+#!/bin/bash
+set -xe
+
+sudo apt-get update -y
+
+sudo apt-get install -y dotnet-sdk-7.0
+
+dotnet --list-sdks
