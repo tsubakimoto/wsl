1b014d1cf362c077ff9f4b07b8f47c650ebf4f24
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2023-07-04 23:53:24 +0900

    Update install-azure-tools.sh

--- install-azure-tools.sh
+++ install-azure-tools.sh
@@ -1,5 +1,6 @@
 # Setup Azure tools on WSL2
-# https://docs.microsoft.com/ja-jp/cli/azure/install-azure-cli-linux
+# https://learn.microsoft.com/ja-jp/cli/azure/install-azure-cli-linux
+# https://learn.microsoft.com/ja-jp/azure/developer/azure-developer-cli/install-azd
 # https://github.com/Azure/azure-functions-core-tools/blob/master/README.md#linux
 
 #!/bin/bash
@@ -9,6 +10,9 @@ sudo apt-get update -y
 
 # Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Azure Developer CLI
+curl -fsSL https://aka.ms/install-azd.sh | sudo bash
 
 # Azure Functions Core Tools
 sudo apt-get update -y
