d3f0cfff7976c7d9e21135e005c7461b0ca4557a
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2023-07-04 22:59:44 +0900

    Update install-dotnet.sh

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -14,13 +14,8 @@ sudo apt-get update && \
 ## https://github.com/dotnet/core/blob/main/release-notes/7.0/install-linux.md
 ## https://github.com/dotnet/sdk/issues/28995#issuecomment-1310911847
 
-# Add package repository
-wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-# Install package
-sudo apt-get update && \
-    sudo apt-get install -y dotnet-sdk-7.0
+mkdir $HOME/dotnet_install && cd $HOME/dotnet_install
+curl -L https://aka.ms/install-dotnet-preview -o install-dotnet-preview.sh
+sudo bash install-dotnet-preview.sh
 
 dotnet --list-sdks
