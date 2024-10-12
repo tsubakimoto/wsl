341fe9a985d94f0d2b171d5d74e51cd9efe1c10d
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2024-10-12 14:55:51 +0900

    Update install-dotnet.sh

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -3,19 +3,18 @@
 #!/bin/bash
 set -xe
 
-# .NET 6 & 7
-## https://learn.microsoft.com/ja-jp/dotnet/core/install/linux-ubuntu#2204
+# .NET 8
+## https://learn.microsoft.com/ja-jp/dotnet/core/install/linux-ubuntu-install
 
 # Install package
 sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-6.0 dotnet-sdk-7.0
+  sudo apt-get install -y dotnet-sdk-8.0
 
-# .NET 8
-## https://github.com/dotnet/core/blob/main/release-notes/8.0/install-linux.md
-## https://github.com/dotnet/sdk/issues/28995#issuecomment-1310911847
+# .NET 9
+## https://github.com/dotnet/core/blob/main/release-notes/9.0/install-linux.md
 
-mkdir $HOME/dotnet_install && cd $HOME/dotnet_install
-curl -L https://aka.ms/install-dotnet-preview -o install-dotnet-preview.sh
-sudo bash install-dotnet-preview.sh
+# mkdir $HOME/dotnet_install && cd $HOME/dotnet_install
+# curl -L https://aka.ms/install-dotnet-preview -o install-dotnet-preview.sh
+# sudo bash install-dotnet-preview.sh
 
 dotnet --list-sdks
