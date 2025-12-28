7c99e8bf11aef6ddcc96629534bca0128e37b652
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2025-12-28 14:24:48 +0900

    Fix path quotes for Git Credential Manager

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo -e "[interop]\nappendWindowsPath=true\n\n[boot]\ncommand=service docker sta
 
 # Git Credential Manager
 # http://microsoft.github.io/Git-Credential-Manager-for-Windows/Docs/CredentialManager.html
-GIT_CREENTIAL_MANAGER_CORE_PATH="/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
+GIT_CREENTIAL_MANAGER_CORE_PATH='/mnt/c/Program Files/Git/mingw64/bin/git-credential-manager.exe'
 #if [ ! -e "$GIT_CREENTIAL_MANAGER_CORE_PATH" ]; then
 #  GIT_CREENTIAL_MANAGER_CORE_PATH="/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
 #fi
