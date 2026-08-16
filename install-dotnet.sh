8f84ffb92c4d06e901314ab8aebdafab8c6a03a5
Yuta Matsumura <tkmbmbr@gmail.com>
2026-08-16 11:54:47 +0900

    Update .NET SDK installation to version 10.0 in install-dotnet.sh

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -3,18 +3,10 @@
 #!/bin/bash
 set -xe
 
-# .NET 8
+# .NET 10
 ## https://learn.microsoft.com/ja-jp/dotnet/core/install/linux-ubuntu-install
 
-# Install package
 sudo apt-get update && \
   sudo apt-get install -y dotnet-sdk-10.0
-
-# .NET 9
-## https://github.com/dotnet/core/blob/main/release-notes/9.0/install-linux.md
-
-# mkdir $HOME/dotnet_install && cd $HOME/dotnet_install
-# curl -L https://aka.ms/install-dotnet-preview -o install-dotnet-preview.sh
-# sudo bash install-dotnet-preview.sh
 
 dotnet --list-sdks
